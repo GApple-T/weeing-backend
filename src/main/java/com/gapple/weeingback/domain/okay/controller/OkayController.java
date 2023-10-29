24db52bb06871aa7b9df088dc4a527267744a782
Donghak Kim <6283hz@Donghaks-MacBook-Pro.local>
2023-10-29 10:47:28 +0900

    ResponseEntity 개선

--- src/main/java/com/gapple/weeingback/domain/okay/controller/OkayController.java
+++ src/main/java/com/gapple/weeingback/domain/okay/controller/OkayController.java
@@ -2,6 +2,8 @@ package com.gapple.weeingback.domain.okay.controller;
 
 import com.gapple.weeingback.domain.okay.service.implementation.OkayServiceImpl;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +14,11 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/okay")
 @RequiredArgsConstructor
 public class OkayController {
-    private final OkayServiceImpl okayService;
+    private final OkayServiceImpl service;
 
     @PostMapping("/submit")
-    public void submitOkay(@Valid OkaySubmitRequest request){
-        okayService.submitOkay(request);
+    public ResponseEntity<Void> submitOkay(@Valid OkaySubmitRequest request){
+        service.submitOkay(request);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }

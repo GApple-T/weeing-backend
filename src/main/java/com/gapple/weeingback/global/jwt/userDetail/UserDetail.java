4c744e155fdbd3dc5d5611b180a22331f7881c34
6283hz <lead@xolving.com>
2023-11-01 22:38:01 +0900

    UserDetail 구성

---
+++ src/main/java/com/gapple/weeingback/global/jwt/userDetail/UserDetail.java
@@ -0,0 +1,50 @@
+package com.gapple.weeingback.global.jwt.userDetail;
+
+import com.gapple.weeingback.domain.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserDetail implements UserDetails {
+    private final User user;
+
+    public UserDetail(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getName();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/gapple/weeingback/global/jwt/userDetail/UserDetailService.java
@@ -0,0 +1,18 @@
+package com.gapple.weeingback.global.jwt.userDetail;
+
+import com.gapple.weeingback.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return new UserDetail(userRepository.findUserByEmail(email));
+    }
+}

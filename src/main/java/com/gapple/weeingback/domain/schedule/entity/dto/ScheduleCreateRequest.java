5d951854cd4486c24aace48c117741553ae4b74a
enbraining <me@xolving.com>
2023-12-18 14:02:05 +0900

    RCequiredArgsConstructor 오류 방지를 위한 인자 하나 추가

--- src/main/java/com/gapple/weeingback/domain/schedule/entity/dto/ScheduleCreateRequest.java
+++ src/main/java/com/gapple/weeingback/domain/schedule/entity/dto/ScheduleCreateRequest.java
@@ -1,12 +1,13 @@
 package com.gapple.weeingback.domain.schedule.entity.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class ScheduleCreateRequest {
     private long classTime;
 }

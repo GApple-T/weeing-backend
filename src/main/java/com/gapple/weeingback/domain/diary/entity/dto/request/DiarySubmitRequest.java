8e5b2a04df2418d1c552a9761d634871d22261bb
enbraining <me@xolving.com>
2023-12-19 11:06:43 +0900

    deprecate된 parser를 parserBuilder로 개선하였습니다.

--- src/main/java/com/gapple/weeingback/domain/diary/entity/dto/request/DiarySubmitRequest.java
+++ src/main/java/com/gapple/weeingback/domain/diary/entity/dto/request/DiarySubmitRequest.java
@@ -1,5 +1,7 @@
 package com.gapple.weeingback.domain.diary.entity.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,11 +10,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DiarySubmitRequest {
+    @NotBlank
     private String title;
 
+    @NotBlank
     private String description;
 
+    @NotNull
     private Long studentGrade;
 
+    @NotNull
     private Long studentClass;
 }

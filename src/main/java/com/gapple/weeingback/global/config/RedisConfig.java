8faa1fc3b425027340537f99cc0804db230227e8
enbraining <me@xolving.com>
2023-12-20 23:48:20 +0900

    레디스 설정 추가

--- src/main/java/com/gapple/weeingback/global/config/RedisConfig.java
+++ src/main/java/com/gapple/weeingback/global/config/RedisConfig.java
@@ -1,2 +1,32 @@
-package com.gapple.weeingback.global.config;public class RedisConfig {
+package com.gapple.weeingback.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+    private final String host;
+    private final int post;
+
+    public RedisConfig(@Value("${spring.data.redis.host}") final String host,
+                       @Value("${spring.data.redis.port}") final int port) {
+        this.host = host;
+        this.post = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, post);
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
 }
